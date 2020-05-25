4b5f33e6a71c7ac34bcbfd5b9df541154b658d1d
wearenothing <39990563+wearenothing@users.noreply.github.com>
2020-05-25 17:19:58 +0800

    Update 10_1.cpp

--- 10_1.cpp
+++ 10_1.cpp
@@ -2,18 +2,22 @@
 #include<vector>
 #include<iterator>
 using namespace std;
-template<class T>
+template<class bili>
 class nnegate
 {
 	public:
-		T operator() (T& t){
+		bili operator() (bili& t){
 			return -t;
 		};
 };
 
-template<class IntputIterator,class OutputIterator,class UnaryFunction >
-OutputIterator transform(IntputIterator first, IntputIterator last, OutputIterator result, UnaryFunction op);
-
+template<class aa,class bb,class cc >
+bb transform(aa first, aa last, bb result,cc op)
+{
+	for(;first!=last;++first,++result)
+	*result=op(*first);
+	return result;
+}
 int main(){
 	const int N=5;
 	vector<int> S(N);
@@ -22,15 +26,5 @@ int main(){
 	transform(S.begin(),S.end(),ostream_iterator<int>(cout," "),nnegate<int>());
 	return 0;
 }
-
-template<class IntputIterator,class OutputIterator,class UnaryFunction >
-OutputIterator transform(IntputIterator first, IntputIterator last, OutputIterator result, UnaryFunction op)
-{
-	for(;first!=last;++first,++result)
-	*result=op(*first);
-	return result;
-}
-
-
 
 
