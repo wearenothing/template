adfedc0d93d8e0e8f79d2eb31de9337fa12026a7
wearenothing <39990563+wearenothing@users.noreply.github.com>
2020-05-25 17:04:10 +0800

    Update 10_1.cpp

--- 10_1.cpp
+++ 10_1.cpp
@@ -1,17 +1,36 @@
 #include<iostream>
 #include<vector>
 #include<iterator>
-#include<algorithm>
-#include<functional>
 using namespace std;
+template<class T>
+class nnegate
+{
+	public:
+		T operator() (T& t){
+			return -t;
+		};
+};
+
+template<class IntputIterator,class OutputIterator,class UnaryFunction >
+OutputIterator transform(IntputIterator first, IntputIterator last, OutputIterator result, UnaryFunction op);
+
 int main(){
 	const int N=5;
 	vector<int> S(N);
 	for(int i=0;i<N;i++)
 	cin>>S[i];
-	transform(S.begin(),S.end(),ostream_iterator<int>(cout," "),negate<int>());
+	transform(S.begin(),S.end(),ostream_iterator<int>(cout," "),nnegate<int>());
 	return 0;
 }
+
+template<class IntputIterator,class OutputIterator,class UnaryFunction >
+OutputIterator transform(IntputIterator first, IntputIterator last, OutputIterator result, UnaryFunction op)
+{
+	for(;first!=last;++first,++result)
+	*result=op(*first);
+	return result;
+}
+
 
 
 
